cc5c3e9ce345902cb5d8e98c95395fcc8980aa34
Rafał Kowalczyk <kowalczykr@trend-glass.local>
2022-05-17 13:05:11 +0200

    Heroku final

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,27 +1,52 @@
-exports.createOne = (Model) => async (req, res, next) => {
-    try {
+function catchAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch((err) => next(err));
+    };
+}
+
+exports.createOne = (Model) =>
+    catchAsync(async (req, res, next) => {
         let doc = await Model.create(req.body);
         res.status(201).json(doc);
-    } catch (err) {
-        next(err);
-    }
-};
+    });
 
-exports.getAll = (Model) => async (req, res, next) => {
-    try {
+exports.getAll = (Model) =>
+    catchAsync(async (req, res, next) => {
         let doc = await Model.find();
         res.status(200).json(doc);
-    } catch (err) {
-        next(err);
-    }
-};
+    });
 
-exports.getOne = (Model) => async (req, res, next) => {
-    try {
+exports.getOne = (Model) =>
+    catchAsync(async (req, res, next) => {
         let doc = await Model.findOne({ _id: req.params._id });
         if (req.params._id) doc = doc.filterLogByQuery(req.query);
         res.status(200).json(doc);
-    } catch (err) {
-        next(err);
-    }
-};
+    });
+
+// exports.createOne = (Model) => async (req, res, next) => {
+//     try {
+//         let doc = await Model.create(req.body);
+//         res.status(201).json(doc);
+//     } catch (err) {
+//         next(err);
+//     }
+// };
+
+// exports.getAll = (Model) => async (req, res, next) => {
+//     try {
+//         let doc = await Model.find();
+//         res.status(200).json(doc);
+//     } catch (err) {
+//         next(err);
+//     }
+// };
+
+// exports.getOne = (Model) => async (req, res, next) => {
+//     try {
+//         let doc = await Model.findOne({ _id: req.params._id });
+//         if (req.params._id) doc = doc.filterLogByQuery(req.query);
+//         res.status(200).json(doc);
+//     } catch (err) {
+//         next(err);
+//     }
+// };
